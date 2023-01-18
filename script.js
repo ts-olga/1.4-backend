2f673fb94d4b657791c38712d987828219bfbc72
Olga Tsyrulyk <olga_tsyrulyk@outlook.com>
2023-01-18 21:31:04 +0800

    Can add new users

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function DataTable(config) {
       })
       .then((data) => {
         //Create table header
-        let tableHtml = `<table>
+        let tableHtml = `<table id="tableId">
+        <div>
+        <button id="addUser" onclick ="addUser()">Add user</button>
+        </div>
       <thead>
           <th>№</th>`;
         for (let i = 0; i < Object.entries(config.columns).length; i++) {
@@ -50,6 +53,72 @@ function deleteUser(userID) {
   }).then(() => {
     DataTable(config1);
   });
+}
+
+function addUser() {
+  if (!document.querySelector("input")) {
+    const row = document.createElement("tr");
+    const td = document.createElement("td");
+
+    td.innerHTML = "";
+    row.appendChild(td);
+
+    for (let i = 0; i < Object.entries(config1.columns).length; i++) {
+      const tdLoop = document.createElement("td");
+      const inp = `<div class="input-container">
+        <input class="input"
+          type="text"
+          id="${config1.columns[i].value}" 
+          name="${config1.columns[i].value}" 
+          placeholder="${config1.columns[i].value}">
+        </div>`;
+      tdLoop.innerHTML = inp;
+      row.appendChild(tdLoop);
+    }
+
+    const add = document.createElement("td");
+    const buttonAdd = `<button class="add-button">Add</button>`;
+    add.innerHTML = buttonAdd;
+    row.appendChild(add);
+
+    document
+      .getElementById("tableId")
+      .childNodes[3].insertBefore(
+        row,
+        document.getElementById("tableId").childNodes[3].firstChild
+      );
+  }
+
+  document.querySelector(".add-button").addEventListener("click", addingUser);
+
+  function addingUser() {
+    // checkInputs();
+
+    let name = document.querySelector("input[name=name]").value;
+    let surname = document.querySelector("input[name=surname]").value;
+    let birthday = document.querySelector("input[name=birthday]").value;
+    let avatar = document.querySelector("input[name=avatar]").value;
+
+    if (name && surname && birthday && avatar) {
+      fetch(config1.apiUrl, {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          surname: surname,
+          avatar: avatar,
+          birthday: birthday,
+        }),
+      })
+        .then((data) => {
+          alert("Added");
+          console.log(data);
+          DataTable(config1);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }
 }
 
 const config1 = {
